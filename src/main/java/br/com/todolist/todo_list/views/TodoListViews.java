5214c3984b01764a010ac032f18107aad2c957ee
Naere Loire Guimarães dos Santos <CIANDT\naere@lnb021851spo.ciandt.global>
2020-12-05 18:34:54 -0300

    add methods views

--- src/main/java/br/com/todolist/todo_list/views/TodoListViews.java
+++ src/main/java/br/com/todolist/todo_list/views/TodoListViews.java
@@ -2,7 +2,14 @@ package br.com.todolist.todo_list.views;
 
 import br.com.todolist.todo_list.models.TodoListModel;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface TodoListViews extends JpaRepository<TodoListModel, Long> {
-    
+    List<TodoListModel> findByStatus(String status);
+
+    @Query("select tag from to_do where title = :title")
+    List<TodoListModel> findTagByTitle(@Param("title") String title);
 }
