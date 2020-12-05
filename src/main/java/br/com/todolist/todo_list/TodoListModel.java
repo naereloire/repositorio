bf215e5ef04b64b887c1e2e32dd413013f532a55
Naere Loire Guimarães dos Santos <CIANDT\naere@lnb021851spo.ciandt.global>
2020-12-05 17:29:08 -0300

    create todo list model

---
+++ src/main/java/br/com/todolist/todo_list/TodoListModel.java
@@ -0,0 +1,67 @@
+package br.com.todolist.todo_list;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class TodoListModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(name = "title", nullable = false)
+    private String title;
+
+    @Column(name = "description", nullable = false)
+    private String description;
+
+    @Column(name = "Status", nullable = false)
+    private String Status;
+
+    @Column(name = "tag", nullable = false)
+    private String tag;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getStatus() {
+        return Status;
+    }
+
+    public void setStatus(String status) {
+        Status = status;
+    }
+
+    public String getTag() {
+        return tag;
+    }
+
+    public void setTag(String tag) {
+        this.tag = tag;
+    }
+}
