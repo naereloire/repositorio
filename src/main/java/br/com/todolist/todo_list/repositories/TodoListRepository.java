ed482912f35e581880fde77f196a68cfaf6ff907
Naere Loire Guimarães dos Santos <CIANDT\naere@lnb021851spo.ciandt.global>
2020-12-23 16:32:50 -0300

    refac inicial

---
+++ src/main/java/br/com/todolist/todo_list/repositories/TodoListRepository.java
@@ -0,0 +1,16 @@
+package br.com.todolist.todo_list.repositories;
+
+import br.com.todolist.todo_list.models.TodoListModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+public interface TodoListRepository extends JpaRepository<TodoListModel, Long> {
+
+    List<TodoListModel> findByStatus(String status);
+
+    @Query(nativeQuery=true , value = "select tag from to_do where title = :title")
+    List<TodoListModel> findTagByTitle(@Param("title") String title);
+}
